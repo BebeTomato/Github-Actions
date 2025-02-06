7d2c1cd002f2c9a2273cb8924c0552456445a47f
Achille Leloup <achille.leloup@epitech.eu>
2025-02-06 11:04:16 +0100

    [TEST] Github action

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // Test memory leak
+    // char *memory_leak = malloc(sizeof(char) * 10);
+    printf("Hello world");
+    return 0;
+}
